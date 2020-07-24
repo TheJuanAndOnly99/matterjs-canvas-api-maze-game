73a83e1d350d1295bfe230bba435318953376952
Juan Estrella <juan.estrella137@gmail.com>
2020-07-24 17:05:34 +0200

    Detect collision between goal and ball

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Engine, Render, Runner, World, Bodies, Body } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
 const cells = 10;
 const width = 600;
@@ -152,14 +152,19 @@ const goal = Bodies.rectangle(
   height - unitLength / 2,
   unitLength * 0.7,
   unitLength * 0.7,
-  { isStatic: true }
+  {
+    isStatic : true,
+    label    : 'goal'
+  }
 );
 
 World.add(world, goal);
 
 // Ball
 
-const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, {
+  label : 'ball'
+});
 World.add(world, ball);
 
 document.addEventListener('keydown', (event) => {
@@ -180,4 +185,16 @@ document.addEventListener('keydown', (event) => {
   if (event.keyCode === 65) {
     Body.setVelocity(ball, { x: x - 5, y });
   }
+});
+
+// Win Condition
+
+Events.on(engine, 'collisionStart', (event) => {
+  Events.pairs.forEach((collision) => {
+    const labels = [ 'ball', 'goal' ];
+
+    if (labels.includes(collision.bodyA.label) && labels.includes(collision.bodyB.label)) {
+      console.log('User won!');
+    }
+  });
 });
