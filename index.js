1c7a75ea3cf209e2b007d6eec5f3d08cf44bca9a
Juan Estrella <juan.estrella137@gmail.com>
2020-07-24 12:49:13 +0200

    Randomly determine maze starting point. Find neighboring cell coordinates and shuffle the results

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -53,8 +54,52 @@ World.add(world, walls);
 
 // Maze Generation
 
-const grid = Array(3).fill(null).map(() => Array(3).fill(false));
+const shuffle = (arr) => {
+  let counter = arr.length;
 
-const verticals = Array(3).fill(null).map(() => Array(2).fill(false));
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
 
-const horizontals = Array(2).fill(null).map(() => Array(3).fill(false));
+    counter--;
+
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+  return arr;
+};
+
+const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
+
+const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+  // If I have visited the call at [column, row], then return
+  if (grid[row][column]) {
+    return;
+  }
+  // Mark this cell as being visited
+  grid[row][column] = true;
+  // Assemble randomly-ordered list of neighbors
+  const neighbors = shuffle([
+    [ row - 1, column ],
+    [ row, column + 1 ],
+    [ row + 1, column ],
+    [ row, column - 1 ]
+  ]);
+
+  // See if that neighbor is out of bounds
+
+  // If we have visited that neighbor, continue to next neighbor
+
+  // Remove a wall from either the horizontals or verticals
+
+  // Visit that next cell
+};
+
+stepThroughCell(1, 1);
